f3bbcbc7bf663317d0b7aadf4a3cbd66d2dc52d3
= <=>
2021-06-07 13:40:20 +0300

    nuxt

--- nuxt.config.js
+++ nuxt.config.js
@@ -24,13 +24,6 @@ export default {
   // Modules for dev and build (recommended) (https://go.nuxtjs.dev/config-modules)
   buildModules: ["@nuxtjs/fontawesome"],
 
-  fontawesome: {
-    component: "fa",
-    icons: {
-      solid: true,
-      brands: true
-    }
-  },
   // Modules (https://go.nuxtjs.dev/config-modules)
   modules: [
     // https://go.nuxtjs.dev/bootstrap
